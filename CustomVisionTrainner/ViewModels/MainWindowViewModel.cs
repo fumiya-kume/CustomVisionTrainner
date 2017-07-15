2faeb84c3e049b8a734549ea61b1ea0bfd374221
Fumiya Kume <fumiya.kume@hotmail.com>
2017-07-15 23:18:21 +0900

    Created ViewModel

--- CustomVisionTrainner/ViewModels/MainWindowViewModel.cs
+++ CustomVisionTrainner/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,14 @@
 ﻿using Prism.Mvvm;
+using Reactive.Bindings;
 
 namespace CustomVisionTrainner.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        public ReactiveProperty<string> CustomVisionKey { get; set; } = new ReactiveProperty<string>();
+        public ReactiveProperty<string> BingImageSearchKey { get; set; } = new ReactiveProperty<string>();
+        public ReactiveProperty<string> EmotionAPIKey { get; set; } = new ReactiveProperty<string>();
+        public ReactiveCommand StartLearningCommand { get; set; } = new ReactiveCommand();
 
         public MainWindowViewModel()
         {
