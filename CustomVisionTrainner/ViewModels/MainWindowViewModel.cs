3680a3062ba84ce080f0d7b33d0b039f200cfd6b
Fumiya Kume <fumiya.kume@hotmail.com>
2017-07-15 22:46:13 +0900

    Project & View Created

--- CustomVisionTrainner/ViewModels/MainWindowViewModel.cs
+++ CustomVisionTrainner/ViewModels/MainWindowViewModel.cs
@@ -4,12 +4,6 @@ namespace CustomVisionTrainner.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Unity Application";
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
 
         public MainWindowViewModel()
         {
